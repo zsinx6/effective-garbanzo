808453f098e2cdb38d9056d0b06bd6e9ffb5e363
zsinx6 <lucassin_x@hotmail.com>
2019-02-20 14:39:04 -0300

    Fixed bills model

--- olist/settings.py
+++ olist/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'records.apps.RecordsConfig'
+    'records.apps.RecordsConfig',
+    'bills.apps.BillsConfig'
 ]
 
 MIDDLEWARE = [
