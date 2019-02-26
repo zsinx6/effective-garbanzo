14a6671cc568fedf6df663e489c3f9a7d71d5ae5
zsinx6 <lucassin_x@hotmail.com>
2019-02-26 15:57:32 -0300

    Fix bills view response

--- bills/views.py
+++ bills/views.py
@@ -15,18 +15,23 @@ class BillInformationViewSet(viewsets.ReadOnlyModelViewSet):
         source = request.query_params.get("source")
         if not source:
             return Response("source is required", 400)
-        month = int(request.query_params.get("month"))
-        year = int(request.query_params.get("year"))
+        month = request.query_params.get("month")
+        year = request.query_params.get("year")
         now = timezone.now()
         first = now.replace(day=1)
         lastMonth = first - datetime.timedelta(days=1)
+        if not year or not month:
+            month, year = lastMonth.month, lastMonth.year
+        else:
+            month, year = int(month), int(year)
         if not 1 <= month <= 12:
             return Response("invalid month", 400)
         if (year > now.year) or (year == now.year and month >= now.month):
             return Response("only closed periods are permitted", 400)
-        if not year or not month:
-            month, year = lastMonth.month, lastMonth.year
         queryset = BillInformation.objects.all()
         queryset = queryset.filter(source=source, end__year=year, end__month=month)
         serializer = BillInformationSerializer(queryset, many=True)
-        return Response(serializer.data)
+        billing = {"source": source,
+                   "period": str(month) + "/" + str(year),
+                   "bills": serializer.data}
+        return Response(billing)
